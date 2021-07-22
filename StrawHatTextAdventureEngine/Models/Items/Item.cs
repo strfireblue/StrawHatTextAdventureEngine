4055712eb362c8d4020553597ae5f460f9b53c77
Rob Michniewicz <strfireblue@gmail.com>
2021-07-21 21:38:19 -0400

    Add project files.

---
+++ StrawHatTextAdventureEngine/Models/Items/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrawHatTextAdventureEngine.Models.Items
+{
+    public class Item
+    {
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ShortDescription { get; set; }
+
+
+
+    }
+}
