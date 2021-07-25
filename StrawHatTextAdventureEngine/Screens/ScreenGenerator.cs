67c36369eef3159cf62780c7d5c5bcfc0aa9065b
Rob Michniewicz <strfireblue@gmail.com>
2021-07-25 14:22:31 -0400

    Screens are generated with color text

--- StrawHatTextAdventureEngine/Screens/ScreenGenerator.cs
+++ StrawHatTextAdventureEngine/Screens/ScreenGenerator.cs
@@ -2,26 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
 using StrawHatTextAdventureEngine.Models.Map;
+using Console = Colorful.Console;
 
 namespace StrawHatTextAdventureEngine.Screens
 {
     public class ScreenGenerator
     {
 
+        private readonly Color COLOR_ROOM_TITLE = Color.SteelBlue;
+        private readonly Color COLOR_DESCRIPTION = Color.White;
+        private readonly Color COLOR_STANDARD_MENU = Color.Gray;
+        private readonly Color COLOR_EXITS = Color.ForestGreen;
+
+
+        /// <summary>
+        /// Renders the game screen, including room title, description, and available actions the player can take.
+        /// </summary>
+        /// <param name="room">The Room the player is currently in.</param>
+        /// <param name="clearScreen">Whether to clear the screen before writing new text.  Default is false.</param>
         public void GenerateScreen(Room room, bool clearScreen = false)
         {
-
-            // Print:
-
-            // Room Title
-            // Room desc, long or short
-            // Standard Command set
-            // Room exits
-            // Room actions
-
-
             if (clearScreen)
                 Console.Clear();
             else
@@ -29,14 +31,15 @@ namespace StrawHatTextAdventureEngine.Screens
                 Console.WriteLine("");
                 Console.WriteLine("");
             }
-                
 
-            Console.WriteLine(room.Name);
-            Console.WriteLine(room.GetDescription());
+
+            Console.WriteLine(room.Name, COLOR_ROOM_TITLE);
+
+            Console.WriteLine(room.GetDescription(), COLOR_DESCRIPTION);
             Console.WriteLine("");
-            Console.WriteLine("Commands: (Tab) Menu   (Esc)  Quit");
+            Console.WriteLine("Commands: (Tab) Menu   (Esc)  Quit", COLOR_STANDARD_MENU);
 
-            room.Exits.ForEach((exit) => Console.WriteLine(GetExitLine(exit)));
+            room.Exits.ForEach((exit) => Console.WriteLine(GetExitLine(exit), COLOR_EXITS));
 
             // TODO: Print room actions (actions don't exist yet)
 
