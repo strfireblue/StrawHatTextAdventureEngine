76bb1240fe0d00550a03dad3be70667a9c4c8c5c
Rob Michniewicz <strfireblue@gmail.com>
2021-07-29 21:53:19 -0400

    Convert quitting and opening menu to IAction classes

---
+++ StrawHatTextAdventureEngine/Models/Actions/QuitAction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using Console = Colorful.Console;
+
+namespace StrawHatTextAdventureEngine.Models.Actions
+{
+    public class QuitAction : IAction
+    {
+        public void Execute()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Thank you for playing!  We hope to see you again soon!", Color.MediumPurple);
+
+            Environment.Exit(0);
+        }
+    }
+}
