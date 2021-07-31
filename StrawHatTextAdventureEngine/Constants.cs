fca1df00a236bfdd2426582b676c562b8c18ac26
Rob Michniewicz <strfireblue@gmail.com>
2021-07-30 22:03:07 -0400

    Add Constants class

---
+++ StrawHatTextAdventureEngine/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrawHatTextAdventureEngine
+{
+    public static class Constants
+    {
+
+        public const string GAME_FLAGS_TOTAL_ACTIONS_TAKEN = "TOTAL_ACTIONS_TAKEN";
+
+
+    }
+}
