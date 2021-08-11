45f7e9b8a89daa32a010d0d89dfd1f5708c3b750
Rob Michniewicz <strfireblue@gmail.com>
2021-08-10 23:20:07 -0400

    Add temporary implementation of Loading GameData

--- StrawHatTextAdventureEngine/Services/Game/GameDataService.cs
+++ StrawHatTextAdventureEngine/Services/Game/GameDataService.cs
@@ -1,13 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using StrawHatTextAdventureEngine.Models.Game;
 using StrawHatTextAdventureEngine.Models.Map;
 
 namespace StrawHatTextAdventureEngine.Services.Game
 {
     public class GameDataService
     {
+
+        public GameData LoadGameData(string pathToGameData)
+        {
+
+            return new GameData()
+            {
+                Player = new Models.Player.Player(),
+                StartingRoom = new Room()
+                {
+                    Name = "TEST ROOM",
+                    Description = "TEST ROOM",
+                    ShortDescription = "TEST ROOM"
+                }
+            };
+        }
+
     }
 }
